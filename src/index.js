bbdfde041e743101cd3b386ab7e650ad1541731e
K.J <117104691+K-Jor162@users.noreply.github.com>
2022-12-18 06:49:29 -0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+let ProfileData;
+let profiles = [
+  {
+    firstName: "Gean",
+    lastName: "Pants",
+    profileName: "Gean777",
+    points: "697"
+  },
+  {
+    firstName: "Dean",
+    lastName: "Winchester",
+    profileName: "Dean477",
+    points: "1297"
+  },
+  {
+    firstName: "Jimmy",
+    lastName: "Lorenzo",
+    profileName: "Jimzo235",
+    points: "1090"
+  }
+];
+console.log(profiles[0].lastName);
